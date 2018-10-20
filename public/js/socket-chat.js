a0eb7e6610e6c198e20972db1d868303a478fa07
AtilaDev <atiladevelop@gmail.com>
2018-10-20 16:57:37 -0300

    Chat casi completo

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -13,42 +13,42 @@ var usuario = {
 };
 
 socket.on('connect', function() {
-  console.log('Conectado al servidor');
+  // console.log('Conectado al servidor');
 
   socket.emit('entrarChat', usuario, function(resp) {
-    console.log('Usuarios conectados', resp);
+    // console.log('Usuarios conectados', resp);
+    renderizarUsuarios(resp);
   });
 });
 
 // escuchar
 socket.on('disconnect', function() {
-  console.log('Perdimos conexión con el servidor');
+  // console.log('Perdimos conexión con el servidor');
 });
 
 // Enviar información
-socket.emit(
-  'enviarMensaje',
-  {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-  },
-  function(resp) {
-    console.log('respuesta server: ', resp);
-  }
-);
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-  console.log('Servidor:', mensaje);
+  // console.log('Servidor:', mensaje);
+  renderizarMensajes(mensaje, false);
+  scrollBottom();
 });
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-  console.log(personas);
+  //   console.log(personas);
+  renderizarUsuarios(personas);
 });
 
 // Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-  console.log(mensaje);
+  console.log('Mensaje Privado:', mensaje);
 });
